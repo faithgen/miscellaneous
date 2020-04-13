338380669da0add87907a9ed0a4b8ad9fc4d119e
innoflash <innocentmazando@gmail.com>
2020-04-13 12:44:33 +0200

    created model, factory for contact

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faithgen\Miscellaneous\Models\Contact;
+use Faker\Generator as Faker;
+
+$factory->define(Contact::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ src/Models/Contact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Faithgen\Miscellaneous\Models;
+
+use FaithGen\SDK\Models\UuidModel;
+
+class Contact extends UuidModel
+{
+    protected $table = 'fg_contact';
+}
