2355a1148090c0d438889d27a73071a316c325ce
innoflash <innocentmazando@gmail.com>
2020-04-13 13:20:09 +0200

    initialized FAQ

---
+++ src/Http/Controllers/FAQController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Faithgen\Miscellaneous\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FAQController extends Controller
+{
+    //
+}
---
+++ src/Models/FAQ.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Faithgen\Miscellaneous\Models;
+
+use FaithGen\SDK\Models\UuidModel;
+
+class FAQ extends UuidModel
+{
+    protected $table = 'fg_f_a_q_s';
+}
